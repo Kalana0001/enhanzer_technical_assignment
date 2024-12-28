aa1b725d0ec957a32d00ae166345b53d22793059
Kalana0001 <141452020+Kalana0001@users.noreply.github.com>
2024-12-28 10:54:44 +0530

    3 commit

--- AdminPanel/src/app/app.component.ts
+++ AdminPanel/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
